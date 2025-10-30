f9c5294820a0f67a92a5174699cb2f31e96c228b
YHJ <0110hj56@gmail.com>
2025-10-30 10:42:17 +0900

    feat: 카메라고정 추가
    카메라 고정추가

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class FollowCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform target;
+    float offsetX;
+
     void Start()
     {
-        
+        if (target == null)
+            return;
+
+        offsetX = transform.position.x - target.position.x;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (target == null)
+            return;
+
+        Vector3 pos = transform.position;
+        pos.x = target.position.x + offsetX;
+        transform.position = pos;
     }
 }
