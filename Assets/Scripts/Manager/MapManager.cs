c04fe7b356126f19bed60383aa1b3ac1c521c15e
YW_Code <l.yeonwu0@gmail.com>
2025-10-30 12:21:40 +0900

    fix: SetObstacle() 코드 수정
    1. 매개변수와 return값을 추가해 SetObstacle의 x값이 배정되지 않는 문제 수정
    2. Top / Bottom의 Obstacle을 각각 받아오는 방식으로 수정

--- Assets/Scripts/Manager/MapManager.cs
+++ Assets/Scripts/Manager/MapManager.cs
@@ -6,24 +6,28 @@ using Random = System.Random;
 
 public class MapManager : MonoBehaviour
 {
+    [SerializeField] private GameObject[] _topObstaclePrefabs;
+    [SerializeField] private GameObject[] _bottomObstaclePrefabs;
+    
     private Random _rand = new Random();
-    private GameObject[] _obstaclePrefabs;
     private SpriteRenderer _spriteRenderers;
     
-    private Vector3 lastPosition = Vector3.zero;
+    private float _widthPaddingObstacle;                // 장애물 좌우 패딩값
+    private float _widthPaddingMinObstacle = 1f;                // 장애물 좌우 패딩 최소값
+    private float _widthPaddingMaxObstacle = 1.5f;              // 장애물 좌우 패딩 최대값
 
-    private float _heightPaddingMin = 1f;
-    private float _heightPaddingMax = 3f;
+    private float _heightPaddingObstacle;               // 장애물 상하 패딩값
+    private float _heightPaddingMinObstacle = 1f;               // 장애물 상하 패딩 최소값
+    private float _heightPaddingMaxObstacle = 1.5f;             // 장애물 상하 패딩 최대값
     
-    private float _widthPaddingObstacle;
-    private float _paddingMin;
-    private float _paddingMax;
-    
-    private float _spawnY;
-    private float _spawnX;
-    private Vector3 _spawnPositionObstacle;
+    private float _spawnX;                              // 장애물 스폰 X 좌표
+    private float _spawnY;                              // 장애물 스폰 Y 좌표
+    private Vector3 _spawnPositionObstacle;             // 장애물 스폰 좌표
 
     private float _widthPaddingItem;
+    private float _widthPaddingMinItem = 1f;
+    private float _widthPaddingMaxItem = 3f;
+    
     private float _spawnPositionItem;
     
     
@@ -34,63 +38,62 @@ public class MapManager : MonoBehaviour
 
     private void Start()
     {
-        if (_obstaclePrefabs.Length > 0)
-            GetObstacle();
+        // if (_obstaclePrefabs.Length > 0)
+        //     GetObstacle();
     }
     
     private void GetObstacle()
     {
-        int index = UnityEngine.Random.Range(0, _obstaclePrefabs.Length);
+        // int index = UnityEngine.Random.Range(0, _obstaclePrefabs.Length);
     }
 
     private void GetItemList()
     {
         
     }
-
-    private void SetObstacle()
+    
+    // 
+    public Vector3 SetObstacle(Vector3 lastPosition)
     {
-        // 프리펩 불러오기
-        int index = UnityEngine.Random.Range(0, _obstaclePrefabs.Length);
-        GameObject obstacle = Instantiate(_obstaclePrefabs[index], _spawnPositionObstacle, Quaternion.identity);
-        
         // 장애물 상하 방향 설정
         int dir = _rand.Next(0, 2) ==  0 ? -1 : 1;
         
+        // 위 / 아래 장애물 랜덤 소환
+        GameObject obstacle;
+        
+        if (dir == 1)
+            obstacle = _topObstaclePrefabs[_rand.Next(0, _topObstaclePrefabs.Length)];
+        else
+            obstacle = _bottomObstaclePrefabs[_rand.Next(0, _bottomObstaclePrefabs.Length)];
+        
         // 중간 패딩 사이즈
-        float heightPadding = UnityEngine.Random.Range(_heightPaddingMin, _heightPaddingMax);
+        _heightPaddingObstacle = UnityEngine.Random.Range(_heightPaddingMinObstacle, _heightPaddingMaxObstacle);
             
         // 장애물의 길이 확인
         SpriteRenderer spriteRenderer = obstacle.GetComponent<SpriteRenderer>();
         float halfHeight = spriteRenderer.bounds.size.y / 2;
-
-        /*// 위에서 소환
-        if (dir == 1)
-        {
-            // 중간 패딩 사이즈
-            float heightPadding = UnityEngine.Random.Range(_heightPaddingMin, _heightPaddingMax);
-            
-            // 장애물의 길이 확인
-            SpriteRenderer spriteRenderer = obstacle.GetComponent<SpriteRenderer>();
-            float halfHeight = spriteRenderer.bounds.size.y / 2;
-            
-            _spawnY = halfHeight + heightPadding;
-        }
-        // 아래에서 소환
+        
+        float screenHalfHeight = Camera.main.orthographicSize;
+        
+        // 오브젝트의 끝이 튀는 것을 방지
+        if (halfHeight + _heightPaddingObstacle > screenHalfHeight)
+            _spawnY = dir * (halfHeight + _heightPaddingObstacle);
         else
-        {
-            // 중간 패딩 사이즈
-            float heightPadding = UnityEngine.Random.Range(_heightPaddingMin, _heightPaddingMax);
-            
-            // 장애물의 길이 확인
-            SpriteRenderer spriteRenderer = obstacle.GetComponent<SpriteRenderer>();
-            float halfHeight = spriteRenderer.bounds.size.y / 2;
-
-            _spawnY = (halfHeight + heightPadding) * dir;
-        }*/
+            _spawnY = dir * (halfHeight + _heightPaddingObstacle * 1.5f);
+        
+        // 장애물 X축 위치 확인
+        _widthPaddingObstacle = UnityEngine.Random.Range(_widthPaddingMinObstacle, _widthPaddingMaxObstacle);
+        _spawnX = lastPosition.x + _widthPaddingObstacle;
+        
+        // 장애물 position 지정
+        _spawnPositionObstacle = new Vector3(_spawnX, _spawnY, 0);
         
         obstacle.transform.position = _spawnPositionObstacle;
-
+        
+        GameObject newObstacle = Instantiate(obstacle, _spawnPositionObstacle, Quaternion.identity);
+        
+        // 마지막 생성 위치 저장
+        return newObstacle.transform.position;
     }
 
     private void SetItem()
